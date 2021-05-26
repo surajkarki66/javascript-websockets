bbc7b952ea9ac4001921ad64b08e765e997d6e25
surajkarki66 <suraj.karki500@protonmail.com>
2021-05-26 23:58:24 +0545

    Finished socker.io-minimal demo

--- socket.io-minimal-demo/app/app.js
+++ socket.io-minimal-demo/app/app.js
@@ -1,14 +1,27 @@
-// Regular websocket
+// Using socket.io
+const socket = io("ws://localhost:8080");
 
-const socket = new WebSocket("ws://localhost:8080");
-console.log("Request connection handshake to server");
-
-// Listen for message
-socket.onmessage = ({ data }) => {
-  console.log(`Message from server ${data}`);
-};
+socket.on("message", (text) => {
+  const el = document.createElement("li");
+  el.innerHTML = text;
+  document.querySelector("ul").appendChild(el);
+});
 
 document.querySelector("button").onclick = () => {
-  console.log("Send message to server");
-  socket.send("hello");
+  const text = document.querySelector("input").value;
+  socket.emit("message", text);
 };
+// // Regular websocket
+
+// const socket = new WebSocket("ws://localhost:8080");
+// console.log("Request connection handshake to server");
+
+// // Listen for message
+// socket.onmessage = ({ data }) => {
+//   console.log(`Message from server ${data}`);
+// };
+
+// document.querySelector("button").onclick = () => {
+//   console.log("Send message to server");
+//   socket.send("hello");
+// };
--- socket.io-minimal-demo/server/index.js
+++ socket.io-minimal-demo/server/index.js
@@ -1,13 +1,28 @@
-// Regular websocket
-const WebSocket = require("ws");
-const server = new WebSocket.Server({ port: "8080" });
+// Socket.io implementations
 
-server.on("connection", (socket) => {
-  console.log("Successfully handshake with client");
-  console.log("Listening income message");
+const http = require("http").createServer();
+const io = require("socket.io")(http, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  console.log("A user is connected");
   socket.on("message", (message) => {
-    console.log("Message get from client");
-    console.log("Sending message back to client");
-    socket.send(`Roger that! ${message}`);
+    console.log(message);
+    io.emit("message", `${socket.id.substr(0, 2)} said  ${message}`);
   });
 });
+
+http.listen(8080, () => console.log("Server is running in PORT 8080"));
+
+// // Regular websocket
+// const WebSocket = require("ws");
+// const server = new WebSocket.Server({ port: "8080" });
+
+// server.on("connection", (socket) => {
+//   console.log("Successfully handshake with client");
+//   console.log("Listening income message");
+//   socket.on("message", (message) => {
+//     console.log("Message get from client");
+//     console.log("Sending message back to client");
+//     socket.send(`Roger that! ${message}`);
+//   });
+// });
